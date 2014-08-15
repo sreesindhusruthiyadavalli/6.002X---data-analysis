9b35085c3a9b9f4edddd64d8a776dd465ebcbea0
sreesindhusruthiyadavalli <sssindh@gmail.com>
2014-08-15 23:44:43 +0530

    Update montyhall.py

--- montyhall.py
+++ montyhall.py
@@ -1,47 +1,221 @@
 import pylab
-import random
 
-def montyChoose(guessDoor, prizeDoor):
-    if 1 != guessDoor and 1 != prizeDoor:
-        return 1
-    if 2 != guessDoor and 2 != prizeDoor:
-        return 2
-    return 3
+#set line width
+pylab.rcParams['lines.linewidth'] = 6
+#set font size for titles 
+pylab.rcParams['axes.titlesize'] = 20
+#set font size for labels on axes
+pylab.rcParams['axes.labelsize'] = 20
+#set size of numbers on x-axis
+pylab.rcParams['xtick.major.size'] = 5
+#set size of numbers on y-axis
+pylab.rcParams['ytick.major.size'] = 5
 
-def randomChoose(guessDoor, prizeDoor):
-    if guessDoor == 1:
-        return random.choice([2,3])
-    if guessDoor == 2:
-        return random.choice([1,3])
-    return random.choice([1,2])
+
+
+def findPayment(loan, r, m):
+    """Assumes: loan and r are floats, m an int
+    Returns the monthly payment for a mortgage of size
+    loan at a monthly rate of r for m months"""
+    return loan*((r*(1+r)**m)/((1+r)**m - 1))
+
+class MortgagePlots(object):
     
-def simMontyHall(numTrials, chooseFcn):
-    stickWins, switchWins, noWin = (0, 0, 0)
-    prizeDoorChoices = [1,2,3]
-    guessChoices = [1,2,3]
-    for t in range(numTrials):
-        prizeDoor = random.choice([1, 2, 3])
-        guess = random.choice([1, 2, 3])
-        toOpen = chooseFcn(guess, prizeDoor)
-        if toOpen == prizeDoor:
-            noWin += 1
-        elif guess == prizeDoor:
-            stickWins += 1
-        else:
-            switchWins += 1
-    return (stickWins, switchWins)
+    def plotPayments(self, style):
+        pylab.plot(self.paid[1:], style, label = self.legend)
+        
+    def plotTotPd(self, style):
+        totPd = [self.paid[0]]
+        for i in range(1, len(self.paid)):
+            totPd.append(totPd[-1] + self.paid[i])
+        pylab.plot(totPd, style, label = self.legend)
 
-def displayMHSim(simResults, title):
-    stickWins, switchWins = simResults
-    pylab.pie([stickWins, switchWins],
-              colors = ['r', 'c'],
-              labels = ['stick', 'change'],
-              autopct = '%.2f%%')
-    pylab.title(title)
 
-simResults = simMontyHall(100000, montyChoose)
-displayMHSim(simResults, 'Monty Chooses a Door')
-pylab.figure()
-simResults = simMontyHall(100000, randomChoose)
-displayMHSim(simResults, 'Door Chosen at Random')
+
+
+class Mortgage(MortgagePlots, object):
+    """Abstract class for building different kinds of mortgages"""
+    def __init__(self, loan, annRate, months):
+        """Create a new mortgage"""
+        self.loan = loan
+        self.rate = annRate/12.0
+        self.months = months
+        self.paid = [0.0]
+        self.owed = [loan]
+        self.payment = findPayment(loan, self.rate, months)
+        self.legend = None #description of mortgage
+
+    def makePayment(self):
+        """Make a payment"""
+        self.paid.append(self.payment)
+        reduction = self.payment - self.owed[-1]*self.rate
+        self.owed.append(self.owed[-1] - reduction)
+
+    def getTotalPaid(self):
+        """Return the total amount paid so far"""
+        return sum(self.paid)
+
+    def __str__(self):
+        return self.legend
+
+
+
+
+
+
+# fixed-rate mortgage
+class Fixed(Mortgage):
+    def __init__(self, loan, r, months):
+        Mortgage.__init__(self, loan, r, months)
+        self.legend = 'Fixed, ' + str(r*100) + '%'
+
+# fixed-rate mortgage with up-front points
+class FixedWithPts(Fixed):
+    def __init__(self, loan, r, months, pts):
+        Fixed.__init__(self, loan, r, months)
+        self.pts = pts
+        self.paid = [loan*(pts/100.0)]
+        self.legend += ', ' + str(pts) + ' points'
+
+# mortgage that changes interest rate after 48 months
+class TwoRate(Mortgage):
+    def __init__(self,loan,r,months,teaserRate,teaserMonths):
+        Mortgage.__init__(self, loan, teaserRate, months)
+        self.teaserMonths = teaserMonths
+        self.teaserRate = teaserRate
+        self.nextRate = r/12.0
+        self.legend = str(teaserRate*100)\
+            + '% for ' + str(self.teaserMonths)\
+            + ' months, then ' + str(r*100) + '%'
+
+    def makePayment(self):
+        if len(self.paid) == self.teaserMonths + 1:
+            self.rate = self.nextRate
+            self.payment = findPayment(self.owed[-1], self.rate,
+                                       self.months - \
+                                       self.teaserMonths)
+        Mortgage.makePayment(self)
+
+
+
+
+
+
+
+
+
+
+
+class MortgagePlots(object):
+    
+    def plotPayments(self, style):
+        pylab.plot(self.paid[1:], style, label = self.legend)
+        
+    def plotTotPd(self, style):
+        totPd = [self.paid[0]]
+        for i in range(1, len(self.paid)):
+            totPd.append(totPd[-1] + self.paid[i])
+        pylab.plot(totPd, style, label = self.legend)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def plotMortgages(morts, amt):
+    styles = ['b-', 'r-.', 'g:']
+    payments = 0 #number to identify a figure
+    cost = 1 #number to identify a figure
+    pylab.figure(payments)
+    pylab.title('Monthly Payments of Different $' + str(amt)
+                + ' Mortgages')
+    pylab.xlabel('Months')
+    pylab.ylabel('Monthly Payments')
+    pylab.figure(cost)
+    pylab.title('Cost of Different $' + str(amt) + ' Mortgages')
+    pylab.xlabel('Months')
+    pylab.ylabel('Total Payments')
+    for i in range(len(morts)):
+        pylab.figure(payments)
+        morts[i].plotPayments(styles[i])
+        pylab.figure(cost)
+        morts[i].plotTotPd(styles[i])
+    pylab.figure(payments)
+    pylab.legend(loc = 'upper center')
+    pylab.figure(cost)
+    pylab.legend(loc = 'best')
+
+
+
+
+
+
+
+
+
+def compareMortgages(amt, years, fixedRate, pts, ptsRate,
+                    varRate1, varRate2, varMonths):
+    totMonths = years*12
+    fixed1 = Fixed(amt, fixedRate, totMonths)
+    fixed2 = FixedWithPts(amt, ptsRate, totMonths, pts)
+    twoRate = TwoRate(amt, varRate2, totMonths,
+                      varRate1, varMonths)
+    morts = [fixed1, fixed2, twoRate]
+    for m in range(totMonths):
+        for mort in morts:
+            mort.makePayment()
+    plotMortgages(morts, amt)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+compareMortgages(amt=200000, years=30, fixedRate=0.07,
+                 pts = 3.25, ptsRate=0.05, varRate1=0.045,
+                 varRate2=0.095, varMonths=48)
+
 pylab.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
