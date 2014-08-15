45ecdb4e2a51b695d91750af7a4d6cee7ec05b6d
sreesindhusruthiyadavalli <sssindh@gmail.com>
2014-08-15 23:20:09 +0530

    Update Combinations.py

--- Combinations.py
+++ Combinations.py
@@ -1,4 +1,28 @@
-def lotsOfParameters2(1,2,3,4,5):
+def combinations(iterable, r):
+    # combinations('ABCD', 2) --> AB AC AD BC BD CD
+    # combinations(range(4), 3) --> 012 013 023 123
+    pool = tuple(iterable)
+    n = len(pool)
+    if r > n:
+        return
+    indices = range(r)
+    yield tuple(pool[i] for i in indices)
+    while True:
+        for i in reversed(range(r)):
+            if indices[i] != i + n - r:
+                break
+        else:
+            return
+        indices[i] += 1
+        for j in range(i+1, r):
+            indices[j] = indices[j-1] + 1
+        yield tuple(pool[i] for i in indices)
+        
+#import itertools
+#x =  itertools.combinations('1234567', 3) 
+
+combinations('1234567', 3)
+       def lotsOfParameters2(1,2,3,4,5):
     print a
     print b
     print c
