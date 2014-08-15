7e38e461738408a1423cf36c8d1b1a8ae2be879c
sreesindhusruthiyadavalli <sssindh@gmail.com>
2014-08-15 23:01:42 +0530

    Create curvefitting1.py

---
+++ curvefitting1.py
@@ -0,0 +1,15 @@
+def lotsOfParameters2(1,2,3,4,5):
+    print a
+    print b
+    print c
+    print d
+    print e
+    
+#lotsOfParameters2()
+lotsOfParameters2(1, 2)
+#lotsOfParameters2(1, c=2)
+
+#lotsOfParameters2(1, e= 20,b = 3)
+#lotsOfParameters2(e=5,d=4,c=3,b=2, 1)
+#lotsOfParameters2(e=5,a=1,d=4,b=2,c=3)
+#lotsOfParameters2(a=5,b=1,c=4,d=2,3)
